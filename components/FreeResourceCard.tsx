b4456ec7c3266228b8aa73b1f200b071e468d670
willstenzel <stenzel.will@gmail.com>
2022-12-08 00:36:40 -0300

    Make free resources mobile friendly

--- components/FreeResourceCard.tsx
+++ components/FreeResourceCard.tsx
@@ -1,7 +1,7 @@
 const FreeResourceCard = ({ title, url, date }) => (
-    <a href={url} target="_blank" rel="noreferrer" className="contact-card mb-3 relative flex flex-row justify-between items-center border-2 border-gray-200 dark:border-gray-800 hover:border-violet-600 hover:dark:border-purple-400 duration-150 rounded-lg p-4">
-        <p className="text-black dark:text-white text-xl sm:text-xl ml-2" style={{ fontWeight: "600" }}>{title}</p>
-        <p className="text-black dark:text-white text-lg sm:text-lg mr-4">{date}</p>
+    <a href={url} target="_blank" rel="noreferrer" className="contact-card mb-3 relative flex flex-col sm:flex-row justify-between items-start sm:items-center border-2 border-gray-200 dark:border-gray-800 hover:border-violet-600 hover:dark:border-purple-400 duration-150 rounded-lg p-4">
+        <p className="text-black dark:text-white text-xl sm:text-xl sm:ml-2" style={{ fontWeight: "600" }}>{title}</p>
+        <p className="text-black dark:text-white text-lg sm:text-lg mr-4 mt-2 sm:mt-0">{date}</p>
     </a>
 );
 
