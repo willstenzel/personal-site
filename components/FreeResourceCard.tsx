cad86c71e1ce5d958ffc20e7990a29660011a958
willstenzel <stenzel.will@gmail.com>
2022-12-11 14:48:33 -0300

    Update border color on FreeResourceCard

--- components/FreeResourceCard.tsx
+++ components/FreeResourceCard.tsx
@@ -1,5 +1,5 @@
 const FreeResourceCard = ({ title, url, date }) => (
-    <a href={url} target="_blank" rel="noreferrer" className="contact-card mb-3 relative flex flex-col sm:flex-row justify-between items-start sm:items-center border-2 border-gray-200 dark:border-gray-800 hover:border-violet-600 hover:dark:border-purple-400 duration-150 rounded-lg p-4">
+    <a href={url} target="_blank" rel="noreferrer" className="contact-card mb-3 relative flex flex-col sm:flex-row justify-between items-start sm:items-center border-2 border-gray-200 dark:border-gray-600 hover:border-violet-600 hover:dark:border-purple-400 duration-150 rounded-lg p-4">
         <p className="text-black dark:text-white text-xl sm:text-xl sm:ml-2" style={{ fontWeight: "600" }}>{title}</p>
         <p className="text-black dark:text-white text-lg sm:text-lg mr-4 mt-2 sm:mt-0">{date}</p>
     </a>
