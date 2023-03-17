91c6ac21239d60107080c1da78b4addc1b7ba4b8
willstenzel <stenzel.will@gmail.com>
2023-03-17 13:05:30 -0500

    Add padding and missing import

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import FreeTemplateCard from 'components/FreeTemplateCard';
 import PastProjectCard from 'components/PastProjectCard';
 import PhotoGallery from 'components/PhotoGallery';
 import RightArrow from 'components/svgs/RightArrow';
+import ContactForm from 'components/ContactForm';
 import ConactCard from 'components/ConactCard';
 import ModernTools from 'components/tooltips/ModernTools';
 import ToolTip from 'components/Tooltip';
@@ -140,7 +141,7 @@ export default function Home() {
   return (
     <Suspense fallback={null}>
       <Container>
-        <div className="flex flex-col justify-center items-start max-w-3xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+        <div className="flex flex-col justify-center items-start max-w-3xl border-gray-200 dark:border-gray-700 mx-auto pb-20">
           <div className="flex flex-col pr-4">
             <h1 className="font-bold text-2xl sm:text-4xl tracking-tight mb-2 text-black dark:text-white">
               Hi! I’m{' '}
